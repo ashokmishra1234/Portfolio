bc62af6c840ae7b11b151353aa83e394b0c1dd1c
Ashok Mishra <ashokmishra1709@gmail.com>
2025-04-10 20:52:42 +0530

    Initial commit

---
+++ Portfolio-main/main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".typed",{
+    strings : ["Frontend Developer", "Sports Man", "Coding Enthusiast","Deep Learning Enthusiast "],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
