f8d2840f088262cfbb709b127cbab46db4c4d7a6
Ashok Mishra <ashokmishra1709@gmail.com>
2025-07-13 03:14:58 +0530

    Updated portfolio with latest files

--- Portfolio-main/main.js
+++
@@ -1,7 +0,0 @@
-var typed= new Typed(".typed",{
-    strings : ["Frontend Developer", "Sports Man", "Coding Enthusiast","Deep Learning Enthusiast "],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-})
